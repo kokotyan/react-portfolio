e393936403544abfa1bca14c6d03375736b17a6f
kokotyan <kokochi.0207@gmail.com>
2023-09-08 12:57:31 +0900

    domを挿入しました。

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Header from './components/Header';
+import Hobby from './components/Hobby';
+import Overview from './components/Overview';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import News from './components/News';
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Header />
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Overview/>}/>
+        <Route path="/" element={<Skills/>}/>
+        <Route path="/" element={<Works/>}/>
+        <Route path="/" element={<Hobby/>}/>
+        <Route path="/" element={<News/>}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
-        <div className="logo">
-            <h3>koko'sportfolio</h3>
-        </div>
+      <div className="logo">
+        <h3>koko'sportfolio</h3>
+      </div>
 
-        <nav>
-            <ul>
-                <li>
-                    <a href="#">overview</a>
-                </li>
-                <li>
-                    <a href="#">skills</a>
-                </li>
-                <li>
-                    <a href="#">works</a>
-                </li>
-                <li>
-                    <a href="#">hobby</a>
-                </li>
-                <li>
-                    <a href="#">news</a>
-                </li>
-            </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">overview</Link>
+          </li>
+          <li>
+            <Link to="/Skills">skills</Link>
+          </li>
+          <li>
+            <Link to="/">works</Link>
+          </li>
+          <li>
+            <Link to="/">hobby</Link>
+          </li>
+          <li>
+            <Link to="/">news</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
