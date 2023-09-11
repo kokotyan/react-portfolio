15310a576cf3aa4ef2b0721e889aca0ae9f02f58
kokotyan <kokochi.0207@gmail.com>
2023-09-11 12:17:42 +0900

    overviewつっ食ったので上げる

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import Header from './components/Header';
-import Hobby from './components/Hobby';
-import Overview from './components/Overview';
-import Skills from './components/Skills';
-import Works from './components/Works';
-import News from './components/News';
-import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Hobby from "./components/Hobby";
+import Overview from "./components/Overview";
+import Skills from "./components/Skills";
+import Works from "./components/Works";
+import News from "./components/News";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Header/>
+      <div className="App">
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Overview/>}/>
-        <Route path="/" element={<Skills/>}/>
-        <Route path="/" element={<Works/>}/>
-        <Route path="/" element={<Hobby/>}/>
-        <Route path="/" element={<News/>}/>
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Overview />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/" element={<Works />} />
+          <Route path="/" element={<Hobby />} />
+          <Route path="/" element={<News />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import myroomImage from './img/my-room.jpg';
+import zidoriImage from './img/zidori.jpg'; 
 
-const Overview = () => {
+function MyComponent() {
   return (
-    <div>Overview</div>
-  )
+    <main id="overview">
+      <div className="list-menu">
+        <div className="content">
+          <img src={myroomImage} alt="部屋の机回り" />
+        </div>
+        <h2>overview</h2>
+        <div className="section">
+          <figure>
+            <img src={zidoriImage} alt="自撮り" /> 
+            <figcaption>2001/02/07 石川県珠洲市にて産声をあげた</figcaption>
+          </figure>
+          <div>
+            <p>金沢出身。</p>
+            <p>訓練中のwebデザイナーの卵</p>
+            <p>空と音楽、それと美味しいものが好き</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Overview
+export default MyComponent;
