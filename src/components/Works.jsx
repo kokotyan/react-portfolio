1afbc05fe1e85e177e2ee38295132d97231b149d
kokotyan <kokochi.0207@gmail.com>
2023-09-18 05:53:24 +0900

    フッターをレスポンシブにしてみました

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,25 +5,37 @@ import KurageImage from './img/kuragechan.png';
 import SuikaImage from './img/suika.png';
 import AkumuImage from './img/akumu.png';
 
+const slides = [
+  {
+    image: KurageImage,
+    alt: 'クラゲ画像',
+    concept: '八月に増えるクラゲ、危ないぞというコンセプトで作りました。',
+    struggles: '苦労した点は毒をどんな感じで表現しようかという単純なものでした。',
+  },
+  {
+    image: SuikaImage,
+    alt: 'スイカ画像',
+    concept: 'これを作った頃は特に熱かったので涼しいものをというコンセプトです。',
+    struggles: '苦戦したことは基本鉛筆で絵を描くのでどうしてもイメージできなかったところです。',
+  },
+  {
+    image: AkumuImage,
+    alt: '銀河街の悪夢',
+    concept: 'そんなに上手ではありませんが、イラストも描けます',
+    struggles: 'このイラストのコンセプトは絶望した少年を描いてみようということで描きました。苦労した点は雨で濡れた肩を描く時の色に困りました。',
+  },
+];
+
 const SlideComponent = () => {
   return (
     <Carousel>
-      <div>
-        <img src={KurageImage} alt='クラゲ画像' />
-        <p>八月に増えるクラゲ、危ないぞというコンセプトで作りました。</p>
-        <p>苦労した点は毒をどんな感じで表現しようかという単純なものでした。</p>
-      </div>
-      <div>
-        <img src={SuikaImage} alt="スイカ画像" />
-        <p>これを作った頃は特に熱かったので涼しいものをというコンセプトです。</p>
-        <p>苦戦したことは基本鉛筆で絵を描くのでどうしてもイメージできなかったところです。</p>
-      </div>
-      <div>
-        <img src={AkumuImage} alt="銀河街の悪夢" />
-        <p>そんなに上手ではありませんが、イラストも描けます</p>
-        <p>このイラストのコンセプトは絶望した少年を描いてみようということで描きました</p>
-        <p>苦労した点は雨で濡れた肩を描く時の色に困りました。</p>
-      </div>
+      {slides.map((slide, index) => (
+        <div key={index}>
+          <img src={slide.image} alt={slide.alt} />
+          <p>{slide.concept}</p>
+          <p>{slide.struggles}</p>
+        </div>
+      ))}
     </Carousel>
   );
 }
