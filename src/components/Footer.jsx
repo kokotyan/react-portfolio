c616cb09ee653918607de72245961448c700bc36
kokotyan <kokochi.0207@gmail.com>
2023-09-19 13:03:36 +0900

    ハンバーガーメニュー作ってみた

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
+  const [showFooter, setShowFooter] = useState(false);
+
+  useEffect(() => {
+    // スクロールイベントを監視してフッターの表示/非表示を制御
+    const handleScroll = () => {
+      const pageHeight = document.documentElement.scrollHeight;
+      const viewportHeight = window.innerHeight;
+      const isAtBottom = window.scrollY + viewportHeight >= pageHeight;
+
+      setShowFooter(isAtBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // コンポーネントがアンマウントされるときにイベントリスナーを削除
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <footer id="footer">
+    <footer id="footer" style={{ display: showFooter ? 'block' : 'none' }}>
       <div className="wrapper">
         <p>© 2023 koko's Portfolio</p>
       </div>
       <div className="sns">
-        <a href="https://twitter.com/murakumo_yu" >
+        <a href="https://twitter.com/murakumo_yu">
           <FontAwesomeIcon icon={faTwitter} />
         </a>
-        <a href="https://github.com/kokotyan" >
+        <a href="https://github.com/kokotyan">
           <FontAwesomeIcon icon={faGithub} />
         </a>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,36 +8,43 @@ const Header = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <header>
       <div className="logo">
         <h3>koko'sportfolio</h3>
       </div>
 
+      {/* スマートフォン用のメニュー */}
       <nav className={`main-menu ${menuOpen ? "open" : ""}`}>
         <ul>
           <li>
-            <Link to="/">overview</Link>
+            <Link to="/" onClick={closeMenu}>overview</Link>
           </li>
           <li>
-            <Link to="/Skills">skills</Link>
+            <Link to="/Skills" onClick={closeMenu}>skills</Link>
           </li>
           <li>
-            <Link to="/Works">Works</Link>
+            <Link to="/Works" onClick={closeMenu}>Works</Link>
           </li>
           <li>
-            <Link to="/Hobby">Hobby</Link>
+            <Link to="/Hobby" onClick={closeMenu}>Hobby</Link>
           </li>
           <li>
-            <Link to="/News">news</Link>
+            <Link to="/News" onClick={closeMenu}>news</Link>
           </li>
         </ul>
       </nav>
 
+      {/* ハンバーガーメニューのトグル */}
       <div className={`menu-toggle ${menuOpen ? "open" : ""}`} onClick={toggleMenu}>
         <div className="bar"></div>
         <div className="bar"></div>
         <div className="bar"></div>
+
       </div>
     </header>
   );
