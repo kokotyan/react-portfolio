397da21d5f46ce70d42154b2d44d72323462dc2b
kokotyan <kokochi.0207@gmail.com>
2023-09-22 05:44:19 +0900

    cnameをパブリックに入れました

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Works from "./components/Works";
 import Hobby from "./components/Hobby";
 import News from "./components/News";
 import Footer from "./components/Footer";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route,Routes,} from "react-router-dom";
 
 function App() {
   return (
